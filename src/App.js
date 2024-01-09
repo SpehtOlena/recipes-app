750a3a6dbcff753491a92ba3e46bc0b330f0a77a
SpehtOlena <speht_olena@ukr.net>
2024-01-09 20:50:17 +0200

    add menu, active page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,39 @@ import Header from './components/Header/Header';
 import DishRecipe from './Pages/DishRecipe/DishRecipe.js';
 import Home from './Pages/Home/Home.js';
 import List from './Pages/List/List.js';
+import Search from './Pages/Search/Search.js';
 import { useState } from 'react';
 
 
+const menuItems = [
+	{
+		label: "Home",
+		element: <Home />
+	},
+	{
+		label: "List",
+		element: <List />
+	},
+	{
+		label: "Search",
+		element: <Search />
+	},
+	{
+		label: "DishRecipe",
+		element: <DishRecipe />
+	}
+
+]
 
 function App() {
-	const [activePage, setActivePage] = useState(null);
+	const [activePage, setActivePage] = useState(<Home />);
 	return (
 		<div className="App">
-			<Header />
-			<Home />
+			<Header items={menuItems} setActivePage={setActivePage} activePage={activePage} />
+			{activePage}
+			{/* <Home />
 			<List />
-			<DishRecipe />
+			<DishRecipe /> */}
 
 		</div>
 	);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,16 @@
 import Logo from '../../styledComponents/Logo'
 import './Header.css'
-import Menu from '../Menu/Menu'
 
-const menuItems = [
-	"Home",
-	"List",
-	"Search"
-]
 
-const Header = () => {
+const Header = ({ setActivePage, items }) => {
+
 	return (
 		<div className={'header'}>
 			<Logo>Recipe</Logo>
-			<Menu menuItems={menuItems} />
+			<div className={'menu-items'}>
+				{items.map((value, index) => <li onClick={() => { setActivePage(value.element) }} key={index} className={'menu-item'}>{value.label.toUpperCase()}</li>)
+				}
+			</div >
 		</div>
 	)
 }
